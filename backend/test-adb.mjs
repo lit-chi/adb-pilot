6f02c0a160d6ab78158e8861d2baed977df0d755
lit-chi <thepuppeteer340@gmail.com>
2025-10-20 01:02:44 +0530

    added new functions

--- backend/test-adb.mjs
+++ backend/test-adb.mjs
@@ -1,4 +1,5 @@
 import { createClient } from "@u4/adbkit";
+import * as fs from 'fs/promises';
 
 const delay = ms => new Promise(res => setTimeout(res,ms));
 
@@ -19,13 +20,34 @@ async function unlockDevice(deviceClient,pin){
     await delay(150);
     command = `input text ${pin} && input keyevent 66`;
     output = await deviceClient.execOut(command,'utf8');
+    if(output.trim().includes('Error')){
+        console.log('failure');
+    }
+    else{
+        console.log(output.trim());
+    }
+}
+
+async function sleep(){
+    const command = 'input keyevent 26';
+    const output = await deviceClient.execOut(command,'utf8');
+}
+
+async function startApp(deviceClient,packageName,activityName){
+    const command = `am start -n ${packageName}/${activityName}`;
+    let output = await deviceClient.execOut(command,'utf8');
 }
 
 async function main(){
     let deviceClient;
     try{
+        const configData = await fs.readFile('./app.config.json','utf8');
+        const apps = JSON.parse(configData);
         deviceClient = await getDeviceClient();
-        await unlockDevice(deviceClient,'3205');
+        //await unlockDevice(deviceClient,'3205');
+        //await delay(1500);
+        await startApp(deviceClient,apps["settings"]["packageName"],apps["settings"]["activityName"]);
+
     }
     catch(err){
         console.error(`Error -> ${err.message}`);
